e1ef2fa30822bcbb8164729ef2d16b5ac7bc6fc0
Jitendar88 <jitendarsingh04@gmail.com>
2025-07-31 14:05:23 +0530

    rg3 (#2)
    added

--- Practice_29jul25/child_module/main.tf
+++ Practice_29jul25/child_module/main.tf
@@ -10,3 +10,7 @@ resource "azurerm_resource_group" "rgjitu2" {
   name = "rgjitu2"
     location = "East US"
 }
+resource "azurerm_resource_group" "rgjitu3" {
+  name = "rgjitu3"
+    location = "East US"
+}
